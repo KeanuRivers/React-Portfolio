893b58cdc1cf40d220f3fcc632f0702876e0f31b
Keanu Rivers <keanurivers404@gmail.com>
2022-11-14 15:37:13 -0700

    updated about me js

---
+++ src/App.js
@@ -0,0 +1,37 @@
+// Required imports
+import React, { Component } from 'react';
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import './App.css';
+// import NavBar from './components/Nav';
+import aboutMe from './components/pages/aboutme';
+// import Projects from './components/pages/Projects';
+// import Footer from './components/Footer';
+// import '@fortawesome/fontawesome-free/css/all.min.css'; 
+import 'bootstrap-css-only/css/bootstrap.min.css'; 
+import 'mdbreact/dist/css/mdb.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import projects from './projects.json';
+
+
+class App extends Component {
+  state = {
+    projects
+  };
+
+  render() {
+    return (
+      
+        <Router basename={process.env.PUBLIC_URL}>
+          {/* <NavBar /> */}
+          <Switch>
+            <Route exact path="/" component={aboutMe} />
+            {/* <Route exact path="/portfolio" render={(props) => <Projects {...props} projects={this.state.projects} />} /> */}
+          </Switch>
+          {/* <Footer /> */}
+        </Router>
+      
+    );
+  };
+};
+
+export default App;
---
+++ src/components/pages/aboutme.js
@@ -0,0 +1,84 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import "./aboutme.css";
+import "../../assets/Resume/Keanu_Resume.pdf";
+
+const aboutMe = () => {
+  return (
+    <div>
+      <Container style={{ width: "75%" }}>
+        <Row>
+          <Col xl={12} lg={12} md={12} sm={12} className="text-center">
+            <h1>
+              <strong>About Me</strong>
+            </h1>
+            <br />
+          </Col>
+        </Row>
+        <Row>
+          <Col lg={4} md={12} sm={12} className="text-center">
+            {/* <img
+              src={require("../../assets/images/IMG_2865.JPG")}
+              alt="Profile"
+              className="img-fluid thumbnail"
+              id="profileimg"
+            /> */}
+          </Col>
+          <Col lg={8} md={12} sm={12} className="text-center">
+            <p>
+              I am a Full Stack Web Developer with an intense passion for code
+              and problem solving. Skilled in HTML, HTML5, CSS, CSS3,
+              JavaScript, jQuery, Bootstrap React.JS, Node.JS, Express, MySQL,
+              MongoDB, Handlebars, Sequelize, AJAX, JSON, Web API. Coding fills
+              me with great satisfaction. I am eager to make this into my life
+              long career choice.
+            </p>
+            <p>
+              "The capacity to learn is a gift; the ability to learn is a skill;
+              the willingness to learn is a choice" -Brian Herbert.
+            </p>
+            <br />
+            <br />
+          </Col>
+        </Row>
+
+        <Row>
+          <Col lg={12} md={12} sm={12} className="text-center" id="icons">
+            {/* <h5>Contact me. I look forward to working with you.</h5>
+            <a href="mailto:keanurivers404@gmail.com" className="left-margin">
+              <i className="fa fa-envelope fa-3x" aria-hidden="false"></i>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/keanu-rivers/"
+              rel="noopener noreferrer"
+              target="_blank"
+              className="left-margin"
+            >
+              <i className="fab fa-linkedin fa-3x"></i>
+            </a>
+            <a
+              href="https://github.com/KeanuRivers"
+              rel="noopener noreferrer"
+              target="_blank"
+              className="left-margin"
+            >
+              <i className="fab fa-github-square fa-3x"></i>
+            </a>
+            <a
+              href={Resume}
+              rel="noopener noreferrer"
+              target="_blank"
+              className="left-margin"
+            >
+              <i className="fas fa-file fa-3x"></i>
+            </a> */}
+            <br />
+            <br />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default aboutMe;
